6cbbcf0d8cbd00131abb44937c7404463ec4530e
Nick <124405252+XenoNick@users.noreply.github.com>
2023-05-12 14:22:29 -0400

    Add some logic for form validation

--- app2.js
+++ app2.js
@@ -1,3 +1,26 @@
-const inputs = [...document.querySelectorAll('.book-info')]
-const addButton = document.querySelector('.add-button')
+const inputs = [...document.querySelectorAll('.book-info')];
+const addButton = document.querySelector('.add-button');
 
+function addErrorMessage(input) {
+  const errorContainer = input.nextElementSibling;
+  errorContainer.classList.add('error-container');
+}
+
+function clearErrorMessage(input) {
+  const errorContainer = input.nextElementSibling;
+  if (errorContainer.classList.contains('error-container')) {
+    errorContainer.classList.remove('error-container');
+  }
+}
+
+addButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  let isValid = true;
+  inputs.forEach((input) => {
+    clearErrorMessage(input);
+    if (!input.checkValidity()) {
+      isValid = false;
+      addErrorMessage(input);
+    }
+  });
+});
