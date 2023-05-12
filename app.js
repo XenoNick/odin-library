e1dff1ed6f3b615b840e8e02eceeb774ecfd508d
Nick <124405252+XenoNick@users.noreply.github.com>
2023-05-12 12:37:08 -0400

    Scrap current JS file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const addButton = document.querySelector('.add-button');
+const inputs = [...document.querySelectorAll('.book-info')];
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -6,3 +8,38 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+
+function createErrorMessage(input) {
+  const parent = input.parentElement;
+  if (!parent.querySelector('.error-container')) {
+    const div = document.createElement('div');
+    div.classList.add('error-container');
+    div.focus();
+    const paragraph = document.createElement('p');
+    paragraph.classList.add('error-text');
+    paragraph.textContent = 'Required';
+    div.append(paragraph);
+    parent.append(div);
+  }
+}
+
+function removeErrorMessage(container) {
+  const errorMessage = container.querySelector('.error-container');
+  if (!errorMessage) return 0;
+  errorMessage.remove();
+  return 0;
+}
+
+addButton.addEventListener('click', () => {
+  // check inputs are valid
+  const result = inputs.forEach((input) => {
+    if (!input.checkValidity()) {
+      // if not valid create error popup
+      createErrorMessage(input);
+      return false;
+    }
+    removeErrorMessage(input.parentElement);
+  });
+  if (result === false) return 0;
+  // if valid create new book entry
+});
