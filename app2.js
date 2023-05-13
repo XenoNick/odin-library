f41cf4295641f29cf68971a1062109ad0cc4f747
Nick <124405252+XenoNick@users.noreply.github.com>
2023-05-13 14:07:10 -0400

    Add the the function to create book entries

--- app2.js
+++ app2.js
@@ -1,5 +1,13 @@
 const inputs = [...document.querySelectorAll('.book-info')];
 const addButton = document.querySelector('.add-button');
+const library = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
 function addErrorMessage(input) {
   const errorContainer = input.nextElementSibling;
@@ -13,6 +21,15 @@ function clearErrorMessage(input) {
   }
 }
 
+function createBookEntry() {
+  const bookValues = inputs.map((input) => {
+    if (input.getAttribute('type') === 'checkbox') return input.checked;
+    return input.value;
+  });
+  const userBook = new Book(...bookValues);
+  library.push(userBook);
+}
+
 addButton.addEventListener('click', (e) => {
   e.preventDefault();
   let isValid = true;
@@ -23,4 +40,7 @@ addButton.addEventListener('click', (e) => {
       addErrorMessage(input);
     }
   });
+  if (isValid) {
+    createBookEntry();
+  }
 });
